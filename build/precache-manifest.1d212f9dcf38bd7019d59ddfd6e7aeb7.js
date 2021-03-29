b38b6569c0ffc7c00740d4a11c7a5355538e376d
Antonio-HTM <ti04@htmeletronica.com.br>
2021-03-29 14:43:49 -0300

    Commit

---
+++ build/precache-manifest.1d212f9dcf38bd7019d59ddfd6e7aeb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333d8105f0715f0a1ba9359438a887e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ad6f9f37ebffaca23bd",
+    "url": "/static/js/2.a66e9e8b.chunk.js"
+  },
+  {
+    "revision": "16ff1bc2e054e8bf2e83e4f19fe0d7ad",
+    "url": "/static/js/2.a66e9e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3dcf04cc4655a5474b7",
+    "url": "/static/js/main.2b76f533.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "48b721cd39140c89030a1b71beb9aa76",
+    "url": "/static/media/logo.48b721cd.png"
+  }
+]);
